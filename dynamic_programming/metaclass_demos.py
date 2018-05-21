a8b013726275ebb064425826f17c6c84aa6b14e7
dongyun.fan <dongyun.fan@mljr.com>
2018-05-21 10:29:09 +0800

    docs: metaclass info and tutorial.

---
+++ dynamic_programming/metaclass_demos.py
@@ -0,0 +1,12 @@
+"""
+参考：
+decorator与metaclass:  http://jfine-python-classes.readthedocs.org/en/latest/index.html
+深入理解python中的metaclass: http://blog.jobbole.com/21351/
+廖雪峰：http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/001386820064557c69858840b4c48d2b8411bc2ea9099ba000
+stackoverflow:http://stackoverflow.com/questions/100003/what-is-a-metaclass-in-python
+monkey_patch:http://stackoverflow.com/questions/5626193/what-is-a-monkey-patch
+python高级技巧之 函数式编程：  http://blog.jobbole.com/66895/
+python高级技巧之  metaclass：  http://blog.jobbole.com/67748/
+  http://jianpx.iteye.com/blog/908121
+  http://py.windrunner.info/design-patterns/singleton.html
+"""
